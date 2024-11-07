4e6ddb5337c27c50fa9e0998944d9a9cf063b2a2
MarcosBB <marcosberaldobarros@hotmail.com>
2024-11-07 09:21:01 -0300

    criando marketplace_books

--- scripts/update-prices.py
+++ scripts/update-prices.py
@@ -7,7 +7,6 @@ https://developers.mercadolivre.com.br/pt_br/itens-e-buscas
 https://api.mercadolibre.com/sites/MLB/categories
 """
 def buscar_preco_livros_mercado_livre(nome):
-    resultado = {}
     base_url = "https://api.mercadolibre.com/sites/MLB/search"
 
     try:
@@ -25,10 +24,12 @@ def buscar_preco_livros_mercado_livre(nome):
         if data['results']:
             # Pega o primeiro item nos resultados
             item = data['results'][0]
-            titulo = item['title']
-            preco = item['price']
-            link = item['permalink']
-            resultado[titulo] = {'preco': preco, 'link': link}
+            return {
+                'price': item['price'],
+                'url': item['permalink'],
+                'image_url': item['thumbnail'],
+                'title': item['title'],
+            }
         else:
              print(f"NÃO ENCONTRADO: {nome}")
     except requests.exceptions.RequestException as e:
@@ -37,7 +38,7 @@ def buscar_preco_livros_mercado_livre(nome):
 
 def get_all_books():
     query = """
-    SELECT livro.titulo, livro.autor, livro.editora, marketplace_book.id FROM livro 
+    SELECT livro.titulo, livro.autor, livro.editora, marketplace_book.id, livro.id FROM livro 
     LEFT JOIN marketplace_book ON livro.id = marketplace_book.livro_id
     """
     cursor.execute(query)
@@ -66,25 +67,12 @@ def atualizar_marketplace_book(livro_id, price, marketplace, url, image_url, tit
         print(f"Nenhum MarketplaceBook encontrado com livro_id {livro_id}.")
 
 def criar_marketplace_book(livro_id, price, marketplace, url, image_url, title):
+    query = """
+    INSERT INTO marketplace_book (price, marketplace, url, image_url, title, livro_id)
+    VALUES (%s, %s, %s, %s, %s, %s)
     """
-    Cria um novo MarketplaceBook se livro_id não existir.
-    """
-    # Verifica se já existe um MarketplaceBook com o livro_id fornecido
-    check_query = "SELECT id FROM MarketplaceBook WHERE livro_id = %s"
-    cursor.execute(check_query, (livro_id,))
-    result = cursor.fetchone()
-
-    if not result:
-        # Se não existe, cria um novo registro
-        insert_query = """
-        INSERT INTO MarketplaceBook (price, marketplace, url, imageUrl, title, livro_id)
-        VALUES (%s, %s, %s, %s, %s, %s)
-        """
-        cursor.execute(insert_query, (price, marketplace, url, image_url, title, livro_id))
-        db_connection.commit()
-        print(f"Novo MarketplaceBook criado com livro_id {livro_id}.")
-    else:
-        print(f"Já existe um MarketplaceBook com livro_id {livro_id}.")
+    cursor.execute(query, (price, marketplace, url, image_url, title, livro_id))
+    db_connection.commit()
 
 
 db_connection = mysql.connector.connect(
@@ -95,13 +83,38 @@ db_connection = mysql.connector.connect(
 )
 cursor = db_connection.cursor()
 
+created_books = 0
+updated_books = 0
+
 livros = get_all_books()
 for livro in livros:
     titulo = livro[0]
     autor = livro[1]
     editora = livro[2]
     marketplace_id = livro[3]
+    id = livro[4]
 
     resultado = buscar_preco_livros_mercado_livre(f"{titulo} - {autor} - {editora}")
-    criar_marketplace_book(marketplace_id, resultado['preco'], 'Mercado Livre', resultado['link'], '', titulo)
-    print(resultado)
+    if marketplace_id:
+        # atualizar_marketplace_book(
+        #     livro_id=id,
+        #     price=resultado['price'],
+        #     marketplace=1,
+        #     url=resultado['url'],
+        #     image_url=resultado['image_url'],
+        #     title=resultado['title']
+        # )
+        updated_books += 1
+    else:
+        criar_marketplace_book(
+            livro_id=id,
+            price=resultado['price'],
+            marketplace=1,
+            url=resultado['url'],
+            image_url=resultado['image_url'],
+            title=resultado['title']
+        )
+        created_books += 1
+
+print(f"{created_books} MarketplaceBooks criados.")
+print(f"{updated_books} MarketplaceBooks atualizados.")
